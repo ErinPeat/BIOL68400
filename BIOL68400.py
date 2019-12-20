d7a74a6a3dba33cc8ef45f5ac78bf6d7882f6520
ErinPeat <erin.peat@nhs.net>
2019-12-20 15:01:36 +0000

    Extra functionality added to error handling. Function created

--- BIOL68400.py
+++ BIOL68400.py
@@ -7,29 +7,36 @@ import numpy as np
 import matplotlib.pyplot as plt 
 #matplotlib is required to generate graphs
 
+import sys
+
 try:
     data = pd.read_csv('antibiotic_data.csv', 
         delimiter=',', header = 0 ,  na_values = ['0', '.'])
-except IOError as e:
-    print('CSV file not found, please ensure you have downloaded the antibiotic_data.csv rom the github repo titled filename and it is in the same folder as the python script')
-#Error handling ensures the CSV file containing the data has been downloaded and prompts users if it cannot be found
+except IOError:
+    print('Error: CSV file not found, please ensure you have downloaded the antibiotic_data.csv from the github repo titled filename and it is in the same folder as the python script')
+    sys.exit(1) #Error handling ensures the CSV file containing the data has been downloaded. Prompts users if it cannot be found and terminates the script.
+
 #Reads in the csv with prescription data,
 #header ensures first row in csv file becomes dataframe headers,
 #na_values: all 0s converted to NaN
 
-null_values = pd.notnull(data['total_list_size'])
-#Labels entries with total list size 0 as False
+def remove_null_values(df): #defining function to remove any null values from the dataframe
 
-removed_entries_list = 0 #Counter for below loop
-for bools in null_values:
-    if bools == False: 
-        removed_entries_list = removed_entries_list + 1
-         #Count of times total list size is 0/NaN
+    null_values = pd.notnull(df['total_list_size'])
+    #Labels entries with total list size 0 as False
 
-final_data = data[null_values] #Removes all values that are labelled as NaN
+    removed_entries_list = 0 #Counter for below loop
+    for bools in null_values:
+        if bools == False: 
+            removed_entries_list = removed_entries_list + 1
+            #Count of times total list size is 0/NaN
 
-print(removed_entries_list , 'entries were deleted due to patient population not reported for this incidence, but GP data may exist in rest of the dataset')
-#Prints number of entries removed so user aware 
+    final_data = df[null_values] #Removes all values that are labelled as NaN
+
+    print(removed_entries_list , 'entries were deleted due to patient population not reported for this incidence, but GP data may exist in rest of the dataset')
+    #Prints number of entries removed so user aware 
+
+remove_null_values(data)
 
 pd.options.mode.chained_assignment = None #The below code is flagging the 'SettingWithCopy' warning, to warn that the
 #operation may be being carried out on a copy. As the output is going into a new column, this is not an issue here.
@@ -60,6 +67,7 @@ axes_line.set_xlabel('Year')
 axes_line.set_title('Mean antibiotic pescriptions per 1000 patients for all Manchester GPs 2015-2018')
 plt.show()
 
+print('Line graph output has been saved to folder')
 
 axes_boxplot = final_data[['items_per_1000', 'name']].boxplot(
                 by='name', figsize=(20, 12), rot=90)
@@ -69,3 +77,4 @@ axes_boxplot.set_ylabel('Items per thousand patients')
 axes_boxplot.set_title('Items per 1000 per practice')
 plt.suptitle('');  # Getting rid of pandas-generated boxplot title
 plt.show() 
+print('Box plot output has been saved to folder')
