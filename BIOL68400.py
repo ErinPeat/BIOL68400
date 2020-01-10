c7394769e485329513024ef29e15f2f5a97f9024
Emily Ley <emilyannette.ley@gmail.com>
2020-01-10 11:31:50 +0000

    csv filename updated, comments added/refined, README updated to reflect changes

--- BIOL68400.py
+++ BIOL68400.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 import sys
 
 try:
-    data = pd.read_csv('items for antibacterial drugs per 1,000 patients on list.csv', 
+    data = pd.read_csv('items_for_antibacterial_drugs_per_1000_patients_on_list.csv', 
         delimiter=',', header = 0 ,  na_values = ['0', '.'])
 except IOError:
-    print('Error: CSV file not found, please ensure you have downloaded the "items for antibacterial drugs per 1,000 patients on list.csv" from the github repo titled filename and it is in the same folder as the python script')
+    print('Error: CSV file not found, please ensure you have downloaded the "items_for_antibacterial_drugs_per_1000_patients_on_list.csv" from the github repo titled filename and it is in the same folder as the python script')
     sys.exit(1) #Error handling ensures the CSV file containing the data has been downloaded. Prompts users if it cannot be found and terminates the script.
 
 #Reads in the csv with prescription data,
@@ -60,25 +60,30 @@ pd.options.mode.chained_assignment = None #warning disabled as above
 
 line_graph = final_data.groupby(pd.Grouper(key= 'date', freq='Y')).mean()['items_per_1000'].plot(legend=True) 
 #grouby groups items from the dataframe as specified. 
-#Grouper seperates the date column and groups everything just by year
-#.mean then finds the mean of each found line and the output of the plot is the items_per_thousand column of the dataframe
+#Grouper seperates the date column and groups everything just by year.
+#.mean then finds the mean of each found line and the output of the plot is the items_per_thousand column of the dataframe.
 
+#The below code generates a line graph of the mean number of prescriptions per 1000 patients across all GP practices for each year.
 axes_line = plt.gca() #gca = get current access
 axes_line.set_ylim([40,60]) #setting value parameters for the Y access
-axes_line.set_ylabel('Mean items per 1000 patients') #Labellong Y and X access and creating a title
+axes_line.set_ylabel('Mean items per 1000 patients') #Labelling Y and X access and creating a title
 axes_line.set_xlabel('Year')
 axes_line.set_title('Mean antibiotic pescriptions per 1000 patients for all Manchester GPs 2015-2018')
-plt.savefig('prescriptions_all_gps_linegraph.png', bbox_inches='tight')
-
+plt.savefig('prescriptions_all_gps_linegraph.png', bbox_inches='tight') 
+#saves the linegraph as a png file. 'tight' = removes white border
 
 print('Line graph output has been saved to folder')
+#informs the user that the line graph can be found in the folder
 
 axes_boxplot = final_data[['items_per_1000', 'name']].boxplot(
                 by='name', figsize=(20, 12), rot=90)
-                #Creating a box plot from dataframe from defined axis
-axes_boxplot.set_xlabel(''); #Setting blank labels and naming others axis
+                #Creating a box plot from final_data dataframe
+axes_boxplot.set_xlabel('') #Setting blank x label, labelling y axis and creating a title 
 axes_boxplot.set_ylabel('Items per thousand patients')
 axes_boxplot.set_title('Items per 1000 per practice')
-plt.suptitle('');  # Getting rid of pandas-generated boxplot title
+plt.suptitle('')  # Getting rid of pandas-generated boxplot title
 plt.savefig('prescriptions_per_gp_boxplot.png', bbox_inches='tight')
+#saves the boxplot as a png file.
+
 print('Box plot output has been saved to folder')
+#informs the user that the boxplot can be found in the folder
--- README.md
+++ README.md
@@ -8,20 +8,20 @@ This code will has been written for the purposes of analysing antibiotic prescri
 
 ### Getting started
 
-Before you start, you will need to download the 'items for antibacterial drugs per 1,000 patients on list.csv' which can be found in the GitHub repository. This is the data required to carry out analysis using this script. 
+Before you start, you will need to download the 'items_for_antibacterial_drugs_per_1000_patients_on_list.csv' which can be found in the GitHub repository. This is the data required to carry out analysis using this script. 
 
 #### Please ensure that you have saved the script and the data in the same folder! The script will not run otherwise!
 
-After addtional packages have been imported, the script will attempt to load data from the 'items for antibacterial drugs per 1,000 patients on list.csv' file and put it into a pandas dataframe
+After addtional packages have been imported, the script will attempt to load data from the 'items_for antibacterial_drugs_per_1000patients_on_list.csv' file and put it into a pandas dataframe
 
-If this is completed successfully, the script will generate the graphs described above. The graphs will be saved as .png files in the same folder as the script and data.
+If this is completed successfully, the script will generate the graphs described above. The graphs will be saved as png files in the same folder as the script and data.
 
 ### Troubleshooting
 
 If the CSV upload does not occur correctly, the script will print: 
 
 ```
-CSV file not found, please ensure that you have downloaded the 'items for antibacterial drugs per 1,000 patients on list.csv' file from the github repo and it is in the same folder as the python script.
+CSV file not found, please ensure that you have downloaded the "items_for_antibacterial_drugs_per_1000_patients_on_list.csv" file from the github repo and it is in the same folder as the python script.
 ```
  
 
@@ -39,7 +39,7 @@ Data is taken from the https://openprescribing.net/analyse/, which selected data
 
 To analyse data from a different CCG, please search for the dataset as described above, but replace 'CCG: NHS MANCHESTER CCG (14L)' with the CCG of your choice.
 
-#### You MUST save the csv file as 'items for antibacterial drugs per 1,000 patients on list.csv' in the same folder as the python script or the script will not run
+#### You MUST save the csv file as 'items_for_antibacterial_drugs_per_1000_patients_on_list.csv' in the same folder as the python script or the script will not run
 
 Data reference: OpenPrescribing.net, EBM DataLab, University of Oxford, 2019
 
